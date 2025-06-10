69f22a664221b749b1645819a8855d6c8e7fb5cf
NithinPatel14 <nithinpatel1002@gmail.com>
2025-06-10 18:11:36 +0530

    conditonal

--- customer.java
+++ customer.java
@@ -1,10 +1,13 @@
 public class customer {
     String coustomer_Name;
     int customerId;
+    int age;
     int customerNumber;
     int payment;
      void getCustomer(){
-        System.out.println(coustomer_Name+ "\n" + customerId + "\n"+ customerNumber+ "\n" +payment);
+        //System.out.println(coustomer_Name+ "\n" + customerId + "\n"+ customerNumber+ "\n" +payment);
+        System.out.println("coustomer_Name: " + coustomer_Name);
+        System.out.println("age: " + age);
     } 
     public static void main(String[] args) {
         customer details = new customer();
@@ -12,6 +15,7 @@ public class customer {
         details.customerId = 786;
         details.customerNumber = 344473823;
         details.payment = 2343;
+        details.age = 30;
         details.getCustomer();
     }
     
